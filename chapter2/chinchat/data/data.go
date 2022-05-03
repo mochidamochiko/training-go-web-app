618aa6cb11f6dc7505b5f3021032a27aff9714d1
mochidamochiko <mochidamochiko@users.noreply.github.com>
2022-05-04 06:25:54 +0900

    postgresのホストを使い分けられるようにenvファイル化

--- chapter2/chinchat/data/data.go
+++ chapter2/chinchat/data/data.go
@@ -3,6 +3,7 @@ package data
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -10,8 +11,11 @@ import (
 var Db *sql.DB
 
 func init() {
-	// pg_host := "172.17.0.2"	// from docker
-	pg_host := "localhost" // from host
+	pg_host := os.Getenv("POSTGRES_HOST")
+	if pg_host == "" {
+		pg_host = "localhost"
+	}
+	log.Printf("PostgreSQL use at %s", pg_host)
 
 	var err error
 	Db, err = sql.Open("postgres", "host="+pg_host+" user=postgres dbname=chinchat sslmode=disable")
